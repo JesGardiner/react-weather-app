e1c55d259db0c3a0731429cbbd67783fee09a8fc
JesGardiner <jes.gardiner21@gmail.com>
2024-08-06 11:11:49 +0100

    Added GitHub repo url

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App"> Weather App
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Jessica Gardiner and is{" "}
+        <a
+          href="https://github.com/JesGardiner/react-weather-app"
+          target="_blank"
+        >
+          {" "}
+          open-sourced on GitHub{" "}
+        </a>
+      </footer>
     </div>
   );
 }
